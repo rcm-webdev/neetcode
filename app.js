d93138df4d7f990fdbbe2e1db362406621b909c2
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-22 13:49:54 -0800

    complete leetcode 125. Valid Palindrome

--- app.js
+++ app.js
@@ -17,3 +17,46 @@ let arr3 = [1, 2, 3, 4, 5, 6];
 console.log(duplicatesPresent(arr1), true);
 console.log(duplicatesPresent(arr2), true);
 console.log(duplicatesPresent(arr3), false);
+
+// 125. Valid Palindrome
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+
+//create a function that is palindome
+
+const isPalindrome = (s) => {
+  let str = s
+    .toLowerCase()
+    .split("")
+    .filter((char) => {
+      return (char >= "a" && char <= "z") || (char >= "0" && char <= "9");
+    })
+    .join("");
+
+  return str == str.split("").reverse().join("");
+};
+
+console.log(isPalindrome(",,.bob.bo,b"), true);
+console.log(isPalindrome("racecar"), true);
+console.log(isPalindrome("racecar.racecar"), true);
+console.log(isPalindrome("bobby"), false);
